d1eb09093a0560999468a96780b6215095dc7497
Celina Louissaint <celinalouissaint@gmail.com>
2021-02-20 17:46:28 -0800

    logic for collecting player name and tracking health points

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,29 @@
 let playerName = window.prompt("What is your robot's name?");
-alert(playerName)
-// this creates a function named "fight"
+let playerHealth = 100;
+let playerAttack = 10;
 
-function fight() {
-    window.alert("The fight has begun!");
-  }
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-  fight();
+let enemyName = "Roborto";
+let enemyHealth = 50;
+let enemyAttack = 12;
+
+let fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+  enemyHealth = enemyHealth - playerAttack;
+  console.log(`${playerName} attacked ${enemyName}, ${enemyName} now has ${enemyHealth} health remaining` );
+// check enemy's health
+    if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+    } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    };
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+      } else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+      };
+};
+
+fight();
