36ce770560d863ad9a29343624b748891ef860fd
Celina Louissaint <celinalouissaint@gmail.com>
2021-02-20 17:21:43 -0800

    added html and linked js file.

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+let playerName = window.prompt("What is your robot's name?");
+alert(playerName)
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+//   fight();
